12e02ecc621ba3d33d58e1262a704447aff1a1a2
pallavideore <37652355+pallavideore@users.noreply.github.com>
2019-07-17 16:03:54 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,15 @@
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=1;
+int b=1;
+int c=a+b;
+//sum of integers
+System.out.println("Sum of the numbers"+c);
+//fdfdgfgfg
+//kkkkk
+	}
+
+}
